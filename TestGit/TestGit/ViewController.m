408cc4a39a2757e40ce5f3a0aef9bb48ed54592b
xzl <xzl@me.com>
2016-02-15 17:41:39 +0800

    modified on master

--- TestGit/TestGit/ViewController.m
+++ TestGit/TestGit/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"modified at 0215");
 }
 
 - (void)didReceiveMemoryWarning {
